085591f2d3d3b0170418d0d05f48f6d0abbdf167
Juliana Crispo <julianacrispo@gmail.com>
2014-10-28 21:20:52 -0400

    images

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141028004409) do
+ActiveRecord::Schema.define(version: 20141029005902) do
 
   create_table "products", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
