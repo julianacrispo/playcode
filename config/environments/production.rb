833b603aa5dd20c9078f9e1d5e241323ed27cc62
Juliana Crispo <julianacrispo@gmail.com>
2014-11-12 20:16:52 -0500

    fixed something for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,6 @@ Rails.application.configure do
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
+
+config.action_mailer.default_url_options = { host: 'jcplaycode.herokuapp.com' }
 end
