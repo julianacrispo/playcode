49a3fd310f1bfe3feb9d8fb79e679d3b332af440
Juliana Crispo <julianacrispo@gmail.com>
2014-11-17 12:58:20 -0500

    added vendor scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141113002629) do
+ActiveRecord::Schema.define(version: 20141117174953) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
@@ -54,5 +54,14 @@ ActiveRecord::Schema.define(version: 20141113002629) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "vendors", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
 
 end
