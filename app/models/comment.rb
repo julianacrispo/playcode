ea1b472e9532185e1f66ecb5a8d6cc74182d183f
Juliana Crispo <julianacrispo@gmail.com>
2014-11-19 13:47:02 -0500

    user show and controller

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :product
   belongs_to :vendor
+  belongs_to :user
 
   # accepts_nested_attributes_for :products, :vendors
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   end
   
   devise_for :users
+  resources :users, only: [:show, :index, :update]
 
 
   resources :products, :path => "brands" do
